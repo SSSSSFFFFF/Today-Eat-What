104164da805f8242abad33bfa0578cb7f1c0cb88
S.F <1074260090@qq.com>
2019-05-07 15:38:28 +0800

    2.0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="loadding" v-loading="loading">
-    <div class="eatWhat">
+
+     <div class="header">
+          <div class="juedingle">今天吃：{{chi}}</div>
+            <el-button class="button-new-tag" size="small" :type="types" @click="chiyige">{{content}}</el-button>
+         
+     </div>
+ <div class="eatWhat">
+                 
+      <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small"
+        @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm" placeholder="新发现">
+      </el-input>
+ <el-button v-else class="button-new-tag" size="small" @click="showInput">添&nbsp;&nbsp;&nbsp;加</el-button>
       <el-tag :key="tag" v-for="tag in dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
         {{tag}}
       </el-tag>
-      <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small"
-        @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm" placeholder="新的发现">
-      </el-input>
-      <el-button v-else class="button-new-tag" size="small" @click="showInput">添 加</el-button>
+
     </div>
-    <div class="juedingle">今天吃：{{chi}}</div>
-      <el-button class="button-new-tag" size="small" @click="chiyige">随机选一个</el-button>
+    
   </div>
 </template>
 
@@ -26,13 +33,16 @@
 		  loading: true,
       qiguai:'',
       chi:'',
+      num: 0,
+      content:'随机选一个',
+      types:'primary',
       }
     },
     mounted() {
       this.axios.get(Host + 'queryTable?db=SF_foods&table=foods').then(response => {
           let json = response.data.data[0];
           console.log("%c 初始查询表的数据", "color:red", json.foodName)
-          this.dynamicTags = json.foodName
+          this.dynamicTags = json.foodName;
           this.loading = false;
 			 this.qiguai = JSON.stringify(this.dynamicTags) 
         })
@@ -58,7 +68,7 @@
         
         let inputValue = this.inputValue;
         if (inputValue) {
-			 this.dynamicTags.push(inputValue);
+			 this.dynamicTags.unshift(inputValue);
 		  }
 
         this.inputVisible = false;
@@ -67,6 +77,7 @@
 		},
 		// 数据库修改新菜
 		change(){
+      console.log("%c 修改数据","color:blue",this.qiguai, this.dynamicTags)
       this.loading = true;
         let postData = {
           "db": "SF_foods",
@@ -88,7 +99,7 @@
             this.axios.get(Host + 'queryTable?db=SF_foods&table=foods').then(response => {
               let json = response.data.data[0];
               console.log("%c 初始查询表的数据", "color:red", json.foodName)
-              this.dynamicTags = json.foodName
+              this.dynamicTags = json.foodName;
               this.loading = false;
                 this.qiguai = JSON.stringify(this.dynamicTags) 
                 this.loading = false;
@@ -104,10 +115,18 @@
     chiyige(){
       var Arr = this.dynamicTags
       var n = Math.floor(Math.random() * Arr.length + 1)-1;  
-      console.log(n)
+      // console.log(n)
       this.chi = this.dynamicTags[n];
-      
-      console.log(this.dynamicTags,this.chi)
+      // console.log(this.dynamicTags,this.chi)
+      this.num ++;
+      console.log(this.num)
+      if(this.num == 3){
+        this.types = 'danger';
+        this.content = '这么矫情，就吃这个！'
+      } else if (this.num > 3){
+        this.types = 'primary';
+        this.content = '你点吧，反正禁止你点也会刷新网页'
+      }
     }
 
     },
@@ -119,11 +138,10 @@
 <style scoped>
   .loadding {
     width: 100vw;
-    height: 100vh;
   }
 
   .eatWhat {
-    padding: 2rem 0.7rem;
+    padding: 2rem 1.5rem;
   }
 
   .el-tag+.el-tag {
@@ -131,23 +149,32 @@
   }
 
   .button-new-tag {
-    margin-left: 10px;
+        min-width: 75px;
     height: 32px;
     line-height: 30px;
-    padding-top: 0;
-    padding-bottom: 0;
+   padding-top: 0;
+     padding-bottom: 0;
   }
 
   .input-new-tag {
-    width: 90px;
-    margin-left: 10px;
-    vertical-align: bottom;
+    width: 75px;
+    vertical-align: center;
   }
 
   body {
     margin: 0;
   }
   .juedingle {
-    margin-top: 10px;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    font-weight: 600;
   }
+  .header {
+     border: 1px solid #cccccc;
+    padding-bottom: 1rem;
+    width: 70vw;
+    margin: 0 auto;
+    margin-top: 2rem;
+  }
+
 </style>
